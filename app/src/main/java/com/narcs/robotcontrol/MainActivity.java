f1c800be53e6de57d0897066d23cf5d7e018bd2c
mmartinez <marcos.martinez@gmail.com>
2018-02-21 23:09:26 +0100

    Working state

--- app/src/main/java/com/narcs/robotcontrol/MainActivity.java
+++ app/src/main/java/com/narcs/robotcontrol/MainActivity.java
@@ -1,8 +1,12 @@
 package com.narcs.robotcontrol;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+
+    private Float vSpeed = new Float(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,53 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        Button ButtonMore = (Button)findViewById(R.id.button);
+        Button ButtonLess = (Button)findViewById(R.id.button2);
+
+        final TextView SpeedTextView = (TextView)findViewById(R.id.sample_text);
+
+        SpeedTextView.setText(Float.toString(vSpeed));
+        // Action Button
+        ButtonMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Context context = getApplicationContext();
+                vSpeed = vSpeed +1;
+                SpeedTextView.setText(Float.toString(vSpeed));
+                CharSequence text = "Hello toast / " + SpeedTextView.getText();
+            }
+        });
+        ButtonLess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Context context = getApplicationContext();
+                vSpeed = vSpeed -1;
+                SpeedTextView.setText(Float.toString(vSpeed));
+                CharSequence text = "Hello toast / " + SpeedTextView.getText();
+            }
+        });
+    // Getters
+
+
+
+
+ //   public void onClick(View v) {
+ //       switch (v.getId()) {
+  //          case  R.id.button: {
+ //               // do something for button 1 click
+ //               break;
+ //           }
+
+ //           case R.id.button2: {
+ //               // do something for button 2 click
+ //               break;
+ //           }
+//
+//            //.... etc
+ //       }
     }
 
     /**
